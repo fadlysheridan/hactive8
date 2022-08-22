17e93aae38ceb6846584e9ddcc436dbb12c6b1fb
fadli.ramadhan <fadli.ramadhan@mncbank.co.id>
2022-08-22 20:53:30 +0700

    update

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	namaBanyak()
+}
+
+func namaBanyak() {
 	names := []string{"fadli", "fisa", "rafi", "kaadek", "kevin", "iqbal", "joko", "budi", "ismed", "david"}
 
 	for i, t := range names {
